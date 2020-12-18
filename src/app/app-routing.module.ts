d330b0dfe72c12963c356da60c049d84d696ea67
gsosamiriam <gsosamiriam@gmail.com>
2020-12-17 20:18:57 -0600

    Paginas con rutas y hash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+import { ProtafolioComponent } from './pages/protafolio/protafolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component: ProtafolioComponent},
+  {path:'about',component: AboutComponent},
+  {path:'item',component: ItemComponent},
+  {path:'**',pathMatch: 'full', redirectTo:'home'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{ useHash:true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
